afffac18712efaa2df36b92eaf8398c597d99f9c
Ivan Ristic <ivanr@webkreator.com>
2017-06-01 22:31:26 +0100

    Added support for multiple View factories in the same application. The first factory that is able to create a view will satisfy the request. By default, we enable both Closure and Velocity templates, in that order.

--- src/main/java/com/webkreator/qlue/example_app/ExampleApplication.java
+++ src/main/java/com/webkreator/qlue/example_app/ExampleApplication.java
@@ -3,8 +3,4 @@ package com.webkreator.qlue.example_app;
 import com.webkreator.qlue.QlueApplication;
 
 public class ExampleApplication extends QlueApplication {
-
-    public ExampleApplication() {
-        //setViewFactory(new SoyViewFactory());
-    }
 }
