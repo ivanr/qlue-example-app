3b8ec6020227483107f8bbd6b6951108baaf46b9
Ivan Ristic <ivanr@webkreator.com>
2017-06-01 21:38:23 +0100

    Rework how response headers are set from the routes file. Because the Servlets specification doesn't allow us to delete response headers, we don't set them on the response directly. Instead, we keep them in a separate structure and set them before a Page runs.

--- src/main/java/com/webkreator/qlue/example_app/ExampleApplication.java
+++ src/main/java/com/webkreator/qlue/example_app/ExampleApplication.java
@@ -1,11 +1,10 @@
 package com.webkreator.qlue.example_app;
 
 import com.webkreator.qlue.QlueApplication;
-import com.webkreator.qlue.view.closure.SoyViewFactory;
 
 public class ExampleApplication extends QlueApplication {
 
     public ExampleApplication() {
-        setViewFactory(new SoyViewFactory());
+        //setViewFactory(new SoyViewFactory());
     }
 }
